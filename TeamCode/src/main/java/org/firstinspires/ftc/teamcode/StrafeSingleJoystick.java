fbc3a8a7da6a446cbfc9083a0ee3b9b923264b2e
PFHS-Robotics <pfhsrobotics@gmail.com>
2023-12-21 17:21:15 -0800

    Updated a bunch of shit in StrafeSingleJoystick.java, if you really want to know what changed then read the code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StrafeSingleJoystick.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StrafeSingleJoystick.java
@@ -1,6 +1,9 @@
-
 package org.firstinspires.ftc.teamcode;
-
+//Math import
+import static java.lang.StrictMath.atan;
+import static java.lang.StrictMath.ceil;
+import static java.lang.StrictMath.sqrt;
+//Robotics import
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -19,13 +22,62 @@ public class StrafeSingleJoystick extends LinearOpMode {
 
     //Other Variables
     boolean moving;
-
+    boolean turning;
+    double speed;
+    double strafeFactor;
+    double strafeSpeed;
+    short theta;
+    double x;
+    double y;
     // Methods
-    void moveStrafe(){
-            BackRight.setPower((4 * gamepad1.left_stick_y) + (4 * gamepad1.left_stick_x));
-            BackLeft.setPower((4 * gamepad1.left_stick_y) + (-4 * gamepad1.left_stick_x));
-            FrontRight.setPower((4 * gamepad1.left_stick_y) + (-4 * gamepad1.left_stick_x));
-            FrontLeft.setPower((4 * gamepad1.left_stick_y) + (4 * gamepad1.left_stick_x));
+    void move(double front, double back){
+            BackRight.setPower(4 * back);
+            BackLeft.setPower(4 * back);
+            FrontRight.setPower(4 * front);
+            FrontLeft.setPower(4 * front);
+    }
+
+    void moveStrafe() {
+        //Variables
+        x = gamepad1.left_stick_x;
+        y = gamepad1.left_stick_y;
+        theta = (short) atan(x/y);
+        int smallTheta = (int) ceil(theta/90);
+        strafeFactor = atan(x/y)/90;
+        speed = sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        strafeSpeed = strafeFactor * speed;
+
+        //Switch function to differentiate between quadrants
+        if(!turning){
+            switch(theta) {
+                case 0:
+                    move(speed, speed);
+                    break;
+                case 90:
+                    move(-speed, speed);
+                    break;
+                case 180:
+                    move(speed, -speed);
+                    break;
+                case 270:
+                    move(-speed, -speed);
+                    break;
+            }
+            switch(smallTheta){
+                case 1:
+                    move(-strafeSpeed, speed);
+                    break;
+                case 2:
+                    move(speed, -strafeSpeed);
+                    break;
+                case 3:
+                    move(strafeSpeed, -speed);
+                    break;
+                case 4:
+                    move(-speed, strafeSpeed);
+                    break;
+            }
+        }
     }
 
     void turn(){
@@ -40,6 +92,7 @@ public class StrafeSingleJoystick extends LinearOpMode {
     void turningWhileMovingFix(){
         //Moving
         moving = gamepad1.left_stick_y != 0 || gamepad1.left_stick_x != 0;
+        turning = gamepad1.left_stick_x != 0;
     }
 
     /**
